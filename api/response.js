850685fab86ade198466239885e4c5be7a98be65
Akporhoke <akporhokea1@gmail.com>
2026-02-06 17:21:01 +0100

    update response.js

--- api/response.js
+++ api/response.js
@@ -3,11 +3,13 @@ export default function handler(req, res) {
     const { yes, noClicks } = req.body;
 
     console.log('User response:');
-    console.log('YES:', yes);
-    console.log('NO clicks:', noClicks);
+    console.log('YES:', yes);        // true if she clicked YES or final CHOICE
+    console.log('NO clicks:', noClicks); // number of times she clicked NO
 
+    // Respond back to frontend
     return res.status(200).json({ received: true });
   }
 
+  // Handle other request methods
   res.status(405).json({ error: 'Method not allowed' });
 }
